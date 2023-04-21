2fb1bba4f133a1344fe1aa002a2701c6fffc5603
shreyash23 <shreyash.agrawal@students.iiit.ac.in>
2023-04-21 23:37:31 +0530

    disk avail updated in chunk server

--- src/chunk_server/util/constants.py
+++ src/chunk_server/util/constants.py
@@ -10,7 +10,8 @@ IP = os.getenv('ip')
 PORT = int(sys.argv[1])
 # PORT = int(os.getenv('port'))
 RAM_AVAIL_GB = int(os.getenv('ram_avail_gb'))
-DISK_AVAIL_GB = int(os.getenv('disk_avail_gb'))
+DISK_AVAIL_GB = 20
+# DISK_AVAIL_GB = int(os.getenv('disk_avail_gb'))
 CHUNK_SERVER_ID = f'CS_{PORT}'
 CHUNK_SERVER_LOCATION_ID = Location_Manager().get_location()
 if len(sys.argv) > 2:
