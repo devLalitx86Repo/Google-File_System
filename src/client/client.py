333f9eb32661fb42d0c65245ea20cc681cf50e95
sarthak-rawat-1101 <sarthak.rawat@students.iiit.ac.in>
2023-04-21 17:16:34 +0530

    added write

--- src/client/client.py
+++ src/client/client.py
@@ -1,15 +1,14 @@
-import sys
-import time
-import json
-import requests
-
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from utils.api_request import post_dict, get_dict
 from utils.gen import make_url
+import requests
+import time
+import json
+import sys
+import os
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 MASTER_URL = ""
+files = dict()
 
 class File:
     def __init__(self, file_name) -> None:
@@ -19,10 +18,10 @@ class File:
         self.last_chunk_id = 0
     
     def request_md(self, chunk_idx):
-        if chunk_idx not in self.chunks or chunk.exp_time > time.time():
+        if chunk_idx not in self.chunks or chunk.exp_time < time.time():
             # request Master: (file_name, chunk_idx) --> chunk_md
             body = {"file_name":file_name, "chunk_idx":chunk_idx}
-            response, status_code = get_dict(MASTER_URL + "/query_chunk/", body)
+            response, status_code = post_dict(MASTER_URL + "/query_chunk", body)
             if status_code == 200:
                 chunk_md = json.load(response.json)
                 chunk = Chunk(chunk_md)
@@ -40,6 +39,10 @@ class File:
         chunk = self.request_md(-1) # -1 denotes last chunk
         return chunk.append(data)
     
+    def write_chunk(self, chunk_idx, byte_start, byte_end, data):
+        chunk = self.request_md(chunk_idx)
+        return chunk.write(data, byte_start, byte_end)
+
     def create_new_chunk(self):
         body = {"file_name":file_name, "chunk_idx":self.last_chunk_id + 1, "checksum":"000"}
         response = requests.post(MASTER_URL + "/query_chunk", json=body)
@@ -49,7 +52,6 @@ class File:
 class Chunk:
     def __init__(self, chunk_md) -> None:
         self.md = chunk_md
-        # self.size = chunk_md["size"]
         self.file_name = chunk_md["fileName"]
         self.chunk_idx = chunk_md["chunkIndex"] 
         self.handle = chunk_md["handle"]
@@ -59,7 +61,6 @@ class Chunk:
         self.exp_time = chunk_md["expiryTime"]
         self.primary_server = chunk_md["primaryServer"]
         # sort chunk_servers wrt client's location
-
         for chi in range(self.replica_count):
             chunk_server_i = {
                 "chunkServerId":"xxx",
@@ -78,10 +79,8 @@ class Chunk:
                 "byteOffset " : byte_range[0],
                 "totalBytes" : byte_range[1],
             }
-            response, status_code = post_dict(chunk_server_url + "/read/", body)
-            if status_code == 200:
-                return response, status_code
-        return None, None
+            print("Read request sent to:", chunk_server_url)
+            return post_dict(chunk_server_url + "/read/", body)
     
     def append(self, data):
         # request primary: (chunk_handle, data) --> (status)
@@ -94,6 +93,7 @@ class Chunk:
             "data" : data,
             "chunkServerInfo" : self.chunk_server_info
         }
+        print("Append request sent to:", chunk_server_url)
         return post_dict(chunk_server_url + "/append/", body)
     
     def write(self, data, byteStart, byteEnd):
@@ -107,15 +107,15 @@ class Chunk:
             "data" : data,
             "chunkServerInfo" : self.chunk_server_info
         }
+        print("Write request sent to:", chunk_server_url)
         return post_dict(chunk_server_url + "/write/", body)
 
 def read_handler(file_name, chunk_idx):
     if file_name not in files:
         new_file = File(file_name)
         files[file_name] = new_file
-
+    print("Read initiated")
     response, status_code = files[file_name].read_chunk(chunk_idx)
-
     if status_code == 200:
         print(response)
     else:
@@ -124,12 +124,12 @@ def read_handler(file_name, chunk_idx):
 def append_handler(file_name, file_path):
     with open(file_path, "rb") as f:
         file_data = f.read()
-    
     if file_name not in files:
         new_file = File(file_name)
         files[file_name] = new_file
-
+    print("Append initiated")
     for attepmts in range(3):
+        print("Attempt#: ", attepmts)
         response, status_code = files[file_name].append_chunk(file_data)
         if status_code == 200:
             print(response)
@@ -139,42 +139,43 @@ def append_handler(file_name, file_path):
             files[file_name].create_new_chunk()
             print("waiting for 5 seconds...")
             time.sleep(5)
-
     print("Cannot append, try again later.")
 
-def write_handler(file_name, file_path):
+def write_handler(file_name, chunk_idx, byte_start, byte_end, file_path):
     with open(file_path, "rb") as f:
         file_data = f.read()
-    
     if file_name not in files:
         new_file = File(file_name)
         files[file_name] = new_file
-
+    print("Write initiated")
     for attepmts in range(3):
-        response, status_code = files[file_name].write_chunk(file_data)
+        print("Attempt#: ", attepmts)
+        response, status_code = files[file_name].write_chunk(file_data, byte_start, byte_end)
         if status_code == 200:
             print(response)
             return
         else:
             print("Error writing data: \n", response)
-            # print("waiting for 5 seconds...")
-            # time.sleep(5)
-
     print("Cannot write, try again later.")
 
-if __name__ == "__main__":
-    files = dict()
+def create_file_handler(file_name):
+    if file_name not in files:
+        new_file = File(file_name)
+        files[file_name] = new_file
+    print("Create initiated")
+    files[file_name].create_new_chunk()
 
+if __name__ == "__main__":
     while True:
         cmd = input().split()
         try:
             if cmd[0] == "read": # read file_name chunk_idx
                 file_name = cmd[1]
-                chunk_idx = cmd[2]
+                chunk_idx = int(cmd[2])
                 read_handler(file_name, chunk_idx)
 
             elif cmd[0] == "write": # write file_name chunk_idx local_file_path
-                pass
+                write_handler(cmd[1], int(cmd[2]), int(cmd[3]), int(cmd[4]), cmd[5])
 
             elif cmd[0] == "append": # append file_name local_file_path
                 file_name = cmd[1]
@@ -182,39 +183,20 @@ if __name__ == "__main__":
                 append_handler(file_name, file_path)
 
             elif cmd[0] == "create":
-                pass
+                file_name = cmd[1]
+                create_file_handler(file_name)
             
             elif cmd[0] == "delete":
                 pass
             
             elif cmd[0] == "setmaster": # setmaster master_url; eg 10.37.155.40:8080
-                MASTER_URL = cmd[1]
+                MASTER_IP = cmd[1]
+                MASTER_PORT = cmd[2]
+                MASTER_URL = make_url(MASTER_IP, MASTER_PORT)
+                
 
             elif cmd[0] == "exit":
                 break
 
         except Exception as e:
             print("Invalid command")    
-
-
-# if __name__ == "__main__":
-#     files = dict()
-
-#     if sys.argv[1] == "read":
-#         file_name = sys.argv[2]
-#         chunk_idx = sys.argv[3] 
-#         read_handler(file_name, chunk_idx)
-    
-#     elif sys.argv[1] == "write":
-#         pass
-    
-#     elif sys.argv[1] == "append":
-#         file_name = sys.argv[2]
-#         file_path = sys.argv[3] 
-#         append_handler(file_name, file_path)
-    
-#     elif sys.argv[1] == "create":
-#         pass
-
-#     elif sys.argv[1] == "delete":
-#         pass
